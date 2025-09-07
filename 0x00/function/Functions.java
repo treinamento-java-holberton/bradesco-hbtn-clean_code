bb7a23ba29e43af0426b62d4d8ee24a62200e285
Arthur Queiroz <arthurclaizoni@gmail.com>
2025-09-07 20:25:29 -0300

    1. Funções

--- 0x00/function/Functions.java
+++ 0x00/function/Functions.java
@@ -21,12 +21,12 @@ public class Functions {
 
     //  No side effects
 
-    //B
+    //A
     public User saveUser(User user){
+        if(user.isAdmin){
+            user.setRole(user);
+        }
         return userRepository.save(user);
-    }
-    public void setRole(User user){
-        user.setRole(user);
     }
 
     // Evite duplicados, extraindo para novos métodos
